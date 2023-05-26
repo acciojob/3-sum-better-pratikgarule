7ecd536c990a2801007f96b5bf7b24efd4bb5a1c
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-05-26 10:22:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
-import java.util.Arrays;
-
-public class ThreeSum {
-    public static int threeSum(int[] arr, int target) {
-        Arrays.sort(arr);
-        int res = arr[0] + arr[1] + arr[2];
-        
-        for (int i = 0; i < arr.length - 2; i++) {
-            int left = i + 1;
-            int right = arr.length - 1;
-            
-            while (left < right) {
-                int closestSum = arr[i] + arr[left] + arr[right];
-                
-                if (Math.abs(closestSum - target) < Math.abs(res - target)) {
-                    res = closestSum;
-                }
-                
-                if (closestSum < target) {
-                    left++;
-                } else if (closestSum > target) {
-                    right--;
-                } else {
-                    return res;
-                }
-            }
-        }
-        
-        return res;
-    }
+function threeSum(arr, target) {
+// write your code here
+  
 }
+
+module.exports = threeSum;
